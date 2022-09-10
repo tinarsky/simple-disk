9f92ef85740723adc3d543c40cb31753197a0f9e
alex-tinarsky <tinarsky@gmail.com>
2022-09-10 16:19:19 +0300

    Init Spring Boot application

---
+++ src/main/java/com/github/tinarsky/SimpleDiskApplication.java
@@ -0,0 +1,11 @@
+package com.github.tinarsky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDiskApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleDiskApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/tinarsky/SimpleDiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tinarsky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDiskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
