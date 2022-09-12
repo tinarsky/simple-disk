74b3c26379fd76d7aa4d3ea298b7a167977e769f
alex-tinarsky <tinarsky@gmail.com>
2022-09-12 13:23:20 +0300

    Improved exception handling

--- src/main/java/com/github/tinarsky/simpledisk/exception_handlers/DefaultExceptionHandler.java
+++ src/main/java/com/github/tinarsky/simpledisk/exception_handlers/DefaultExceptionHandler.java
@@ -5,8 +5,15 @@ import com.github.tinarsky.simpledisk.exceptions.NotFoundException;
 import com.github.tinarsky.simpledisk.models.ClientError;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
@@ -29,12 +36,20 @@ public class DefaultExceptionHandler extends ResponseEntityExceptionHandler {
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(BadRequestException.class)
 	public ClientError handleBadRequestException(BadRequestException e) {
-		return badRequestError;
+		return new ClientError(400, e.getMessage());
 	}
 
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	@ExceptionHandler(NotFoundException.class)
 	public ClientError handleNotFoundException(NotFoundException e) {
 		return notFoundError;
+	}
+
+	@NonNull
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(
+			@NonNull HttpMessageNotReadableException ex, @NonNull HttpHeaders headers,
+			@NonNull HttpStatus status, @NonNull WebRequest request) {
+		return new ResponseEntity<>(badRequestError, HttpStatus.BAD_REQUEST);
 	}
 }
