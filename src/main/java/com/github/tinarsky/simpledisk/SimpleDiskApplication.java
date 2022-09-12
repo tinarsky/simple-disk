1421a7b0a6ac918510d43297d11021ecf0b99bae
alex-tinarsky <tinarsky@gmail.com>
2022-09-12 13:18:18 +0300

    Added property source

--- src/main/java/com/github/tinarsky/simpledisk/SimpleDiskApplication.java
+++ src/main/java/com/github/tinarsky/simpledisk/SimpleDiskApplication.java
@@ -2,8 +2,10 @@ package com.github.tinarsky.simpledisk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:db.properties")
 public class SimpleDiskApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleDiskApplication.class, args);
