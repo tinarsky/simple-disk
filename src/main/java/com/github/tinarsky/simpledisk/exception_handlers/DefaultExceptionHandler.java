33549da94ebfdcbc238c8cb3c9f55c3bc17bdc86
alex-tinarsky <tinarsky@gmail.com>
2022-09-13 00:43:11 +0300

    Added some new exception handlers

--- src/main/java/com/github/tinarsky/simpledisk/exception_handlers/DefaultExceptionHandler.java
+++ src/main/java/com/github/tinarsky/simpledisk/exception_handlers/DefaultExceptionHandler.java
@@ -10,10 +10,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.lang.NonNull;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
@@ -49,6 +53,38 @@ public class DefaultExceptionHandler extends ResponseEntityExceptionHandler {
 	@Override
 	protected ResponseEntity<Object> handleHttpMessageNotReadable(
 			@NonNull HttpMessageNotReadableException ex, @NonNull HttpHeaders headers,
+			@NonNull HttpStatus status, @NonNull WebRequest request) {
+		return new ResponseEntity<>(badRequestError, HttpStatus.BAD_REQUEST);
+	}
+
+	@NonNull
+	@Override
+	protected ResponseEntity<Object> handleMissingServletRequestParameter(
+			@NonNull MissingServletRequestParameterException ex, @NonNull HttpHeaders headers,
+			@NonNull HttpStatus status, @NonNull WebRequest request) {
+		return new ResponseEntity<>(badRequestError, HttpStatus.BAD_REQUEST);
+	}
+
+	@NonNull
+	@Override
+	protected ResponseEntity<Object> handleMissingPathVariable(
+			@NonNull MissingPathVariableException ex, @NonNull HttpHeaders headers,
+			@NonNull HttpStatus status, @NonNull WebRequest request) {
+		return new ResponseEntity<>(badRequestError, HttpStatus.BAD_REQUEST);
+	}
+
+	@NonNull
+	@Override
+	protected ResponseEntity<Object> handleNoHandlerFoundException(
+			@NonNull NoHandlerFoundException ex, @NonNull HttpHeaders headers,
+			@NonNull HttpStatus status, @NonNull WebRequest request) {
+		return new ResponseEntity<>(badRequestError, HttpStatus.BAD_REQUEST);
+	}
+
+	@NonNull
+	@Override
+	protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
+			@NonNull HttpRequestMethodNotSupportedException ex, @NonNull HttpHeaders headers,
 			@NonNull HttpStatus status, @NonNull WebRequest request) {
 		return new ResponseEntity<>(badRequestError, HttpStatus.BAD_REQUEST);
 	}
